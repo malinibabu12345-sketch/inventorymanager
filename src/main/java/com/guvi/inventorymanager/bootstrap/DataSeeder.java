0d37b7b8ff99297fdcff0051950e97ffc1a33ccf
Malini <malinibabu12345@gmail.com>
2026-05-07 22:36:39 +0530

    Fix

--- src/main/java/com/guvi/inventorymanager/bootstrap/DataSeeder.java
+++ src/main/java/com/guvi/inventorymanager/bootstrap/DataSeeder.java
@@ -7,7 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class DataSeeder implements CommandLineRunner {
 
     private final UserRepository userRepository;
