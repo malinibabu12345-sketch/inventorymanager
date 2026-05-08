7a3b854c869b519a0940e51e6237b94d568933be
Malini <malinibabu12345@gmail.com>
2026-05-08 10:55:00 +0530

    final

--- src/main/java/com/guvi/inventorymanager/dto/AuthRequest.java
+++ src/main/java/com/guvi/inventorymanager/dto/AuthRequest.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.NotBlank;
 
 public class AuthRequest {
 
+    @NotBlank(message = "Name is required")
+    private String username;
+
     @Email(message = "Invalid email format")
     @NotBlank(message = "Email is required")
     private String email;
@@ -13,6 +16,10 @@ public class AuthRequest {
     private String password;
 
     public AuthRequest() {}
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
 
     public String getEmail() {
         return email;
